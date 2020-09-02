115d386a590a0536f62af1cb44e2efc76de9c4d8
EricksonLopes <ofc.erickson@gmail.com>
2020-09-02 18:16:00 -0300

    Requests na prática

--- introducao/app.py
+++ introducao/app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
+
+# indicando a aplicação
 app = Flask(__name__)
 
 
+# Criando Rota
 @app.route("/")
 def index():
-    return 'Página inicial'
+    return "<a href='/posts'>Posts</a>"
+
+
+@app.route("/posts")
+@app.route("/posts/<int:id>")
+def posts(id):
+
+    # ?titulo=mensagem
+    titulo = request.args.get("titulo")
+
+    data = dict(
+        path=request.path,
+        referrer=request.referrer,
+        content_type=request.content_type,
+        method=request.method,
+        titulo=titulo,
+        id=id
+    )
+    return jsonify(data)
 
 
 if __name__ == '__main__':
