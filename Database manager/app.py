28d3d01af264f1f9193fcdd7975520ae04ae7fcb
EricksonLopes <ofc.erickson@gmail.com>
2020-09-03 10:43:11 -0300

    configurando sqlalchemy

--- Database manager/app.py
+++ Database manager/app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# parâmetros obrigadorio para o sqlalchemy rodas na aplicação
+db = SQLAlchemy(app)
+
+# tipo de banco de dados que ira usar / sqlite3
+app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
 
 
 @app.route("/")
