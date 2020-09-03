8b009818718b74905fd4e5243a8ba8bc753e34c4
EricksonLopes <ofc.erickson@gmail.com>
2020-09-03 10:35:37 -0300

    Introdução

---
+++ Database manager/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return 'Página em branco'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
