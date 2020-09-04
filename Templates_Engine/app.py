503fd8dfe01e834350ab30763613dd47718cc325
EricksonLopes <ofc.erickson@gmail.com>
2020-09-04 17:28:26 -0300

    Iterando com os objetos

--- Templates_Engine/app.py
+++ Templates_Engine/app.py
@@ -6,7 +6,12 @@ app = Flask(__name__, template_folder='templates_aula')
 
 @app.route('/templates')
 def templates():
-    return render_template('index.html')
+    users = {
+        'name': 'Marcus',
+        'anos': 99,
+        'email': 'oi@gmauil.com'
+    }
+    return render_template('index.html', users=users)
 
 
 if __name__ == '__main__':
