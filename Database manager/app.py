2acb1dabe13aa64cd1b5f507fb8b3b6cb4b33871
EricksonLopes <ofc.erickson@gmail.com>
2020-09-04 14:01:22 -0300

    Logando Usuários

--- Database manager/app.py
+++ Database manager/app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin
+from flask_login import LoginManager, UserMixin, login_required, login_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
 # tipo de banco de dados que ira usar / sqlite3
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secret'
 
 # parâmetros obrigadorio para o sqlalchemy rodas na aplicação
 db = SQLAlchemy(app)
@@ -62,9 +64,51 @@ def delete(id):
 
 
 @app.route('/user/<int:id>')
+@login_required
 def index_user(id):
     user = User.query.filter_by(id=id).first()
     return render_template('user.html', user=user)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        user = User()
+        user.name = request.form['name']
+        user.email = request.form['email']
+        user.password = generate_password_hash(request.form['password'])
+
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    errors = {}
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+        errors = {}
+
+        # se não existir redireciona
+        if not user:
+            flash('Credênciais inválidas.')
+            return redirect(url_for('login'))
+
+        # verifica se a senha esta correta
+        if not check_password_hash(user.password, password):
+            flash('Credênciais inválidas.')
+            return redirect(url_for('login'))
+
+        login_user(user)
+        return redirect(url_for('index'))
+
+    return render_template('login.html', errors=errors)
 
 
 if __name__ == '__main__':
