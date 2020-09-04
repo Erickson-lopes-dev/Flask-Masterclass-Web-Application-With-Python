c2f532707f1bf7b689deccb4e28719c0e04b5dd0
EricksonLopes <ofc.erickson@gmail.com>
2020-09-04 17:49:41 -0300

    flash

--- Templates_Engine/app.py
+++ Templates_Engine/app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash
 
 app = Flask(__name__, template_folder='templates_aula')
-
+app.config['SECRET_KEY'] = 'sserve'
 
 @app.route('/templates')
 def templates():
@@ -11,7 +10,10 @@ def templates():
         'anos': 99,
         'email': 'oi@gmauil.com'
     }
-    return render_template('index.html', users=users)
+
+    flash("Usuário criado com sucesso!")
+    # flash("passei por aqui!")
+    return render_template('index.html', user=users)
 
 
 if __name__ == '__main__':
